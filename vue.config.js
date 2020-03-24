4c2c022377a6683787314e61f8337ce53bf5541b
sayil1 <sundaysayil4u@gmail.com>
2020-03-24 13:58:15 +0100

    add no-cache settings in vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -23,5 +23,13 @@ module.exports = {
       swSrc: 'src/registerServiceWorker.js',
       // ...other Workbox options...
     }
+  },
+  chainWebpack: config => {
+    if (process.env.NODE_ENV === 'production') {
+      config.module.rule('vue').uses.delete('cache-loader');
+      config.module.rule('js').uses.delete('cache-loader');
+      config.module.rule('ts').uses.delete('cache-loader');
+      config.module.rule('tsx').uses.delete('cache-loader');
+    }
   }
 }
